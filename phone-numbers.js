de7fad2272e03cdf0d1a417f7defa8aa1de4ddf8
Jorge Luis <jorge.luis4212@gmail.com>
2019-08-18 14:25:16 -0400

    add phone numbers

---
+++ phone-numbers.js
@@ -0,0 +1,7 @@
+// add () - to 0123456789
+// sperate the numbers in the string to their own element
+// before or after the number element add () -, dont forget the space after )
+
+function createPhoneNumber(numbers){
+    return ( "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9])
+  }
